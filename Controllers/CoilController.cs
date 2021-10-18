060db70e713ec302869d9859b9cf3b4b2fc3bf89
Northon T. Cavazzoni <northoncavazzoni@hotmail.com>
2021-10-18 16:40:35 -0300

    Update
    Update

--- Controllers/CoilController.cs
+++ Controllers/CoilController.cs
@@ -9,8 +9,7 @@ namespace MyAPI.Controllers;
 [Route("[controller]")]
 public class CoilController : ControllerBase
 {
-    [HttpGet]
-    public IEnumerable<Coil> Get() //IEnumerable<Coil> Get()
+    private IEnumerable<Coil> GetAllCoils()
     {
         //Supported spreadsheet formats for reading include: XLSX, XLS, CSV and TSV
         WorkBook workbook = WorkBook.Load("D:\\Repos\\Temp\\MyAPI\\Data\\DEC -XCR - 04_09_2020 - 10dias.xls");
@@ -19,31 +18,68 @@ public class CoilController : ControllerBase
         //Convert the worksheet to System.Data.DataTable
         //Boolean parameter sets the first row as column names of your table.
         DataTable dataTable = sheet.ToDataTable(true);
-        
+
         //The DataTable can populate a DataGrid for example.
 
         List<Coil> coils = new List<Coil>();
 
+        int id = 0;
+        double weight = 0.0;
+        double thickness = 0.0;
+        double width = 0.0;
+        double length = 0.0;
+
         //Enumerate by rows or columns first at your preference
         foreach (DataRow row in dataTable.Rows)
         {
+            bool idParse = int.TryParse(row["IDC_XCOIL_REP"].ToString(), out id); 
+            bool weightParse = double.TryParse(row["WG_ME_COIL"].ToString(), out weight);
+            bool thicknessParse = double.TryParse(row["TH_ME_STRIP"].ToString(), out thickness);
+            bool lengthParse = double.TryParse(row["LG_XCOIL"].ToString(), out length);
+            bool widthParse = double.TryParse(row["WD_XCOIL"].ToString(), out width);
 
-            System.Console.WriteLine("IDC: " + row["IDC_XCOIL_REP"].ToString());
-            
-            Coil coil = new Coil()
+            if (idParse && weightParse && thicknessParse && lengthParse && widthParse)
             {
-                Id = int.Parse(row["IDC_XCOIL_REP"].ToString()),
-                Name = row["COIL_ID"].ToString(),
-                Weight = double.Parse(row["WG_ME_COIL"].ToString()),
-                Thickness = double.Parse(row["TH_ME_STRIP"].ToString()),
-                Length = double.Parse(row["LG_XCOIL"].ToString()),
-                Width = double.Parse(row["WD_XCOIL"].ToString())                
-            };
-            
-            coils.Add(coil);
+                Coil coil = new Coil()
+                {
+                    Id = id,
+                    Name = row["COIL_ID"].ToString(),
+                    Weight = weight,
+                    Thickness = thickness,
+                    Length = length,
+                    Width = width
+                };
+                coils.Add(coil);
+            }
+
         }
 
         return coils;
     }
-    
+
+    [HttpGet]
+    public string VerifyAndReport(string name)
+    {
+        Coil? coil = GetAllCoils().Where(x => x.CheckWeight() != 0).FirstOrDefault();
+
+        if (coil != null)
+        {
+            switch (coil.CheckWeight())
+            {
+                case 1:
+                    return "Peso acima do limite tolerado. Excedendo " + (coil.GetMaxWeightTolerated() - coil.Weight).ToString("F2") + " Kg.";
+                case -1:
+                    return "Peso abaixo do limite tolerado. Faltando " + (coil.GetMinWeightTolerated() - coil.Weight).ToString("F2") + " Kg.";
+                case 0:
+                    return "Peso dentro dos limites tolerados.";
+                default:
+                    return "Validação de peso não realizada!";
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Bobina " + name + " não encontrada!");
+        }
+    }
+
 }
--- Models/Coil.cs
+++ Models/Coil.cs
@@ -13,5 +13,36 @@ public class Coil
     public double Length { get; set; }
 
     public double Width { get; set; }
+
+    private double _tolerance = 5;
+    private double _density = 0.00785;
+
+    public int CheckWeight ()
+    {
+        switch (Weight)
+        { 
+            case var value when value > GetMaxWeightTolerated():
+                return 1; //"Peso acima do limite tolerado. Excedendo " + (GetMaxWeightTolerated()-Weight).ToString() + " Kg.";
+
+            case var value when value < GetMinWeightTolerated():
+                return -1; //"Peso abaixo do limite tolerado. Faltando " + (GetMinWeightTolerated()-Weight).ToString() + " Kg.";
+
+            default:
+                return 0; //"Peso dentro dos limites tolerados.";
+        
+        }
+    } 
+
+    public double GetMaxWeightTolerated(){
+        return Length * Width * Thickness * _density * (1 + _tolerance/100);
+    }
+
+    public double GetMinWeightTolerated(){
+        return Length * Width * Thickness * _density * (1 - _tolerance/100);
+    }
+
+    public double GetWeightCalculated(){ 
+        return Length * Width * Thickness * _density;
+    }
     
 }
